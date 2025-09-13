ce23a8a1f3089c0cfec4a9fffe438aa569a4e3d1
pargame <117472201+pargame@users.noreply.github.com>
2025-09-14 01:20:51 +0900

    chore: add compatibility shims for old vendor/index asset filenames

---
+++ public/assets/vendor.react-DAVwNj0q.js
@@ -0,0 +1,4 @@
+// Compatibility shim for old deployed filename
+// This file forwards the old vendor.react-DAVwNj0q.js request to the current vendor.react bundle.
+// It will be served as a JS module by GitHub Pages.
+import '/MyBlog/assets/vendor.react-0XcSIb80.js';
---
+++ public/assets/vendor.react-dom-3tj8Pi-E.js
@@ -0,0 +1,2 @@
+// Compatibility shim for old deployed filename
+import '/MyBlog/assets/vendor.react-dom-3tj8Pi-E.js';
